d530185b3aa40fe7b83a8b38dd4f16ced54ae15a
joaovitorangelo <joaovitorangelo05@gmail.com>
2023-10-06 20:39:25 -0300

    responsive adjust

--- navbar.js
+++ navbar.js
@@ -1,6 +1,12 @@
 //NAVBAR
 let navigation = document.querySelector('.navbar');
 let toggle = document.querySelector('.toggle');
-toggle.onclick = function(){
-    navigation.classList.toggle('active')
-}
+    
+    toggle.onclick = function(){
+        navigation.classList.toggle('active')
+    }
+
+    navigation.onclick = function(){
+        navigation.classList.toggle('active')
+    }
+

4de89e6b4ce9d13d8ed17b3bae387bb878c70f7f
joaovitorangelo <joaovitorangelo05@gmail.com>
2023-10-07 00:17:09 -0300

    teste quatro

--- navbar.js
+++ navbar.js
@@ -5,8 +5,3 @@ let toggle = document.querySelector('.toggle');
     toggle.onclick = function(){
         navigation.classList.toggle('active')
     }
-
-    navigation.onclick = function(){
-        navigation.classList.toggle('active')
-    }
-

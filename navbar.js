b7a49c54b4c19afb11a4c1b422f29a924ccbe7ea
joaovitorangelo <joaovitorangelo05@gmail.com>
2023-10-07 02:28:55 -0300

    teste dez

--- navbar.js
+++ navbar.js
@@ -5,3 +5,14 @@ let toggle = document.querySelector('.toggle');
     toggle.onclick = function(){
         navigation.classList.toggle('active')
     }
+
+//navbarmobile
+let navigationMobile = document.querySelector('.navbarMobile');
+let toggleMobile = document.querySelector('.toggleMobile');
+    
+    toggleMobile.onclick = function(){
+        navigationMobile.classList.toggle('activeMobile');
+    }
+    navigationMobile.onclick = function(){
+        navigationMobile.classList.toggle('activeMobile');
+    }
